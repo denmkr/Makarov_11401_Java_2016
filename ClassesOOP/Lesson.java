90cba4c5890e0cdf7e23f0135f699f88759c4cb8
Denis Makarov <den.mkr@gmail.com>
2016-02-26 10:45:06 +0300

    added classes

---
+++ ClassesOOP/Lesson.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Denis on 26.02.16.
+ */
+public class Lesson {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
---
+++ ClassesOOP/Student.java
@@ -0,0 +1,39 @@
+import java.util.List;
+
+/**
+ * Created by Denis on 26.02.16.
+ */
+public class Student {
+    private List<Lesson> lessons;
+    private String name;
+    private Institute institute;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setInstitute(Institute institute) {
+        this.institute = institute;
+    }
+
+    public Institute getInstitute() {
+        return institute;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + "study in" + institute;
+    }
+}
